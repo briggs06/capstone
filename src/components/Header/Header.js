2cfce4d1db761a244cfb230b452e8043cf1c5809
briggs06 <148621947+briggs06@users.noreply.github.com>
2024-06-14 22:28:41 -0400

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.scss";
+import Navbar from "./Navbar";
+import SearchForm from "./SearchForm";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Navbar />
+      <div className='header-content flex align-center justify-center flex-column text-center'>
+        <SearchForm />
+        <h1 className='text-white header-title ls-2'>"More food, more life!"</h1>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
